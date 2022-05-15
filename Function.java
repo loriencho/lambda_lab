70f11eb0040559f3c9e01ad2c1d87c49669c13c4
Satwika Vemuri <satwika.vemuri@gmail.com>
2022-05-15 10:05:37 -0400

    questions

--- Function.java
+++ Function.java
@@ -1,9 +1,11 @@
 
 public class Function implements Expression {
-    Variable operator;
+    Variable var;
+    Expression exp;
 
-    public Function(Variable operator){
-        this.operator = operator;
+    public Function(Variable var, Expression exp){
+        this.var = var;
+        this.exp = exp;
     }
     
     public static boolean isFunction(String str){
@@ -11,6 +13,14 @@ public class Function implements Expression {
 	}
 
     public String toString(){
-        return operator.toString();
+        return "λ" + var + "." + exp;
     }
 }
+
+/*
+questions to ask:
+1. Can you explain where the variable and where the expression in function comes rom
+2. in λa.a, what does the tree structure look like? like what does the application look like and what is the function's expression and variable?
+3. In the console should we able to type both / and λ and it will read as lambda?
+4. And how do we get the lambda to print and not the question marks?
+*/
